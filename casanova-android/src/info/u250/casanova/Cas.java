606950d6e1604a5dc287bbff2b475b79a5351152
lycying@gmail.com <lycying@gmail.com>
2014-06-09 12:20:24 +0800

    the apks

--- casanova-android/src/info/u250/casanova/Cas.java
+++ casanova-android/src/info/u250/casanova/Cas.java
@@ -14,48 +14,54 @@ import com.badlogic.gdx.backends.android.surfaceview.RatioResolutionStrategy;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
-import com.google.android.gms.ads.InterstitialAd;
 
 public class Cas extends AndroidApplication {
 	private static final String XID = "ca-app-pub-0429520335266267/9030641132";
 	protected AdView adView;
 	private LoadingProgressAndroid loadingView;
 	RelativeLayout layout;
+
 	public LoadingProgressAndroid getLoadingView() {
 		return loadingView;
 	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		layout = new RelativeLayout(this);
 		loadingView = new LoadingProgressAndroid(this);
-		
+
 		adView = new AdView(this); // Put in your secret key here
-        adView.setAdUnitId(XID);
-        adView.setAdSize(AdSize.BANNER);
-        adView.loadAd(new AdRequest.Builder().build());
-        
+		adView.setAdUnitId(XID);
+		adView.setAdSize(AdSize.BANNER);
+
 		Casanova game = new Casanova(new LoadingFeedbackAndroid(this));
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		config.resolutionStrategy = new RatioResolutionStrategy(Engine.getWidth(), Engine.getHeight());
-		
+		config.resolutionStrategy = new RatioResolutionStrategy(
+				Engine.getWidth(), Engine.getHeight());
+
 		View main = initializeForView(game, config);
 		layout.addView(main);
-		RelativeLayout.LayoutParams adParams = 
-                new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,RelativeLayout.LayoutParams.WRAP_CONTENT);
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_TOP);
-        adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+		RelativeLayout.LayoutParams adParams = new RelativeLayout.LayoutParams(
+				RelativeLayout.LayoutParams.WRAP_CONTENT,
+				RelativeLayout.LayoutParams.WRAP_CONTENT);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+		adParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 
-        layout.addView(adView, adParams);
+		layout.addView(adView, adParams);
 		setContentView(layout);
 		loadingView.showLoading();
-		
-		InterstitialAd mInterstitial = new InterstitialAd(this);
-        mInterstitial.setAdUnitId(XID);
-        mInterstitial.loadAd(new AdRequest.Builder().build());
-        mInterstitial.show();
+
+		// InterstitialAd mInterstitial = new InterstitialAd(this);
+		// mInterstitial.setAdUnitId(XID);
+		// mInterstitial.loadAd(new AdRequest.Builder().build());
+		// mInterstitial.show();
+
+		adView.loadAd(new AdRequest.Builder().addTestDevice(
+				AdRequest.DEVICE_ID_EMULATOR).build());
 
 	}
+
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
@@ -66,5 +72,31 @@ public class Cas extends AndroidApplication {
 			return true;
 		}
 		return false;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (adView != null) {
+			adView.resume();
+		}
+	}
+
+	@Override
+	public void onPause() {
+		if (adView != null) {
+			adView.pause();
+		}
+		super.onPause();
+	}
+
+	/** Called before the activity is destroyed. */
+	@Override
+	public void onDestroy() {
+		// Destroy the AdView.
+		if (adView != null) {
+			adView.destroy();
+		}
+		super.onDestroy();
 	}
 }
